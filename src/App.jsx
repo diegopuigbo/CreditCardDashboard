dd78727c0949d8a2bd7675714391b0daea2d8287
diegopuigbo <diegopuigbo22@gmail.com>
2024-03-01 10:17:24 +0100

    fixing jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
+// App.jsx
 import { useState, useEffect } from 'react';
 import CreditCard from './components/CreditCard';
 import useCreditCards from './components/useCreditCards';
-
 
 const App = () => {
   const [quantity, setQuantity] = useState(1);
   const [cardTypeFilter, setCardTypeFilter] = useState(null);
   const [searchValue, setSearchValue] = useState('');
   const { creditCards, fetchCreditCards } = useCreditCards();
+  const [cardCount, setCardCount] = useState({});
 
   useEffect(() => {
     fetchCreditCards(quantity);
   }, [quantity]);
+
+  useEffect(() => {
+    const newCardCount = {};
+
+    creditCards.map((card) => {
+      if (newCardCount[card.type]) {
+        newCardCount[card.type]++;
+      } else {
+        newCardCount[card.type] = 1;
+      }
+      return null;
+    });
+
+    setCardCount(newCardCount);
+  }, [creditCards]);
+
 
   const handleCardTypeFilter = (type) => {
     setCardTypeFilter(type);
@@ -51,14 +68,15 @@ const App = () => {
             Cantidad:
             <input type="number" value={quantity} onChange={(event) => setQuantity(event.target.value)} />
           </label>
-          <button onClick={() => handleCardTypeFilter('Visa')}>Visa</button>
-          <button onClick={() => handleCardTypeFilter('MasterCard')}>Mastercard</button>
-          <button onClick={() => handleCardTypeFilter('American Express')}>American Express</button>
-          <button onClick={() => handleCardTypeFilter('Discover Card')}>American Express</button>
+          {Object.keys(cardCount).map((type) => (
+            <button key={type} onClick={() => handleCardTypeFilter(type)}>
+              {type} ({cardCount[type]})
+            </button>
+          ))}
           <button onClick={() => setCardTypeFilter(null)}>Clear Filter</button>
         </div>
         <div className="content">
-          {filteredCreditCards.map((data, index) => (
+          {cardTypeFilter && filteredCreditCards.map((data, index) => (
             <CreditCard key={index} data={data} />
           ))}
         </div>
